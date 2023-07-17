1a925b1547452f8414b36acff1c8f22587de043f
David Szabados <dv5150dev@gmail.com>
2023-07-17 16:09:44 +0200

    payment provider support

--- src/Resources/PaymentModeResource.php
+++ src/Resources/PaymentModeResource.php
@@ -2,14 +2,18 @@
 
 namespace DV5150\Shop\Filament\Resources;
 
+use DV5150\Shop\Contracts\Models\PaymentModeContract;
+use DV5150\Shop\Facades\Shop;
 use DV5150\Shop\Filament\Resources\PaymentModeResource\Pages\CreatePaymentMode;
 use DV5150\Shop\Filament\Resources\PaymentModeResource\Pages\EditPaymentMode;
 use DV5150\Shop\Filament\Resources\PaymentModeResource\Pages\ListPaymentModes;
+use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 
 class PaymentModeResource extends Resource
@@ -29,6 +33,8 @@ class PaymentModeResource extends Resource
 
     public static function form(Form $form): Form
     {
+        $currency = config('shop.currency.code');
+
         return $form
             ->schema([
                 TextInput::make('name')
@@ -36,17 +42,33 @@ class PaymentModeResource extends Resource
                 TextInput::make('price_gross')
                     ->nullable()
                     ->numeric()
-                    ->label('Gross Price (' . config('shop.currency.code') . ')'),
+                    ->label("Gross Price ($currency)"),
+                Checkbox::make('is_active')
+                    ->label('Active'),
             ]);
     }
 
     public static function table(Table $table): Table
     {
+        $currency = config('shop.currency.code');
+
         return $table
             ->columns([
                 TextColumn::make('name'),
                 TextColumn::make('price_gross')
-                    ->label('Gross Price (' . config('shop.currency.code') . ')'),
+                    ->label("Gross Price ($currency)"),
+                IconColumn::make('is_active')
+                    ->boolean()
+                    ->label('Active'),
+                IconColumn::make('is_online_payment')
+                    ->boolean()
+                    ->label('Online Payment'),
+                IconColumn::make('is_installed')
+                    ->boolean()
+                    ->getStateUsing(function (\stdClass $state, PaymentModeContract $record) {
+                        return Shop::hasPaymentProvider($record->getProvider());
+                    })
+                    ->label('Installed')
             ])
             ->filters([
                 //
